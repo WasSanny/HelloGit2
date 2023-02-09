c2af6d04d654a2ea270188aed10b21b1c892270f
Dmitry Vasiliuk <usy222@mail.ru>
2023-02-09 23:36:26 +0300

    add RExamle 003 Test3

---
+++ Example003_Test3/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Hello, World!");
+Console.WriteLine("1 Hello, World!");
+Console.WriteLine("2 Hello, World!");
+Console.WriteLine("3 Hello, World!");
+Console.WriteLine("4 Hello, World!");
+Console.WriteLine("5 Hello, World!");
+Console.WriteLine("6 Hello, World!");
+Console.WriteLine("7 Hello, World!");
